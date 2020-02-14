79da7294940acf4cffe3e6173083d8160308ecbd
Student <Student@nwu.ac.za>
2020-02-14 12:44:40 +0200

    my new sql file;

---
+++ student.sql
@@ -0,0 +1,19 @@
+show database
+
+create database
+use student;
+
+CREATE TABLE STUDENTS(
+ID INT NOT NULL,
+NAME VARCHAR(20),
+AGE INT NOT NULL,
+COURSE CHAR(20) NOT NULL,
+PRIMARY KEY (ID));
+
+desc students
+
+git status
+
+git add .
+
+git commit -m "my new sql file";
